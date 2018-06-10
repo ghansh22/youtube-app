21c1fcc96dbd2764e9e7ceed65b750864e4335c4
ghanshyam22 <ghanshyam@freeabillion.com>
2018-06-10 14:21:40 +0530

    react states

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,9 +4,25 @@ class SearchBar extends Component {
   // onInputChange(eventObject) {
   //   console.log(eventObject.target.value);
   // }
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
 
   render() {
-    return <input onChange={eventObject => console.log(eventObject.target.value)} />
+    return (
+      <div>
+        <input onChange={eventObject => this.setState({
+          term: eventObject.target.value
+        })}
+        />
+        <h2>
+          Value of the input : {this.state.term}
+        </h2>
+      </div>
+    )
   }
 }
 
