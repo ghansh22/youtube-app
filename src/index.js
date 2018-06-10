27c8eca997129f118532c9deae66c304b13e0bb6
ghanshyam22 <ghanshyam@freeabillion.com>
2018-06-10 17:22:45 +0530

    video list rendering

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = '';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = {
+      videos: []
+    };
+    YTSearch({
+      key: API_KEY,
+      term: 'Google Pixel'
+    }, (videos) => {
+      this.setState({
+        videos: videos
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
