e0da0aa5ce7b76f0c1ec5acfe1fb926875798f2e
ghanshyam22 <ghanshyam@freeabillion.com>
2018-06-10 18:18:29 +0530

    video selection works

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,8 +3,13 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   // debugger
-  const videoItems =  props.videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video}></VideoListItem>
+  const videoItems = props.videos.map((video) => {
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video} />
+    );
   });
 
   return (
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const VideoListItem = (props) => {
   const video = props.video;
+  const onVideoSelect = props.onVideoSelect;
   console.log(video)
   return (
-    <li className="list-group-item">
+    <li onClick={ () => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" src={video.snippet.thumbnails.default.url}/>
+          <img className="media-object" src={video.snippet.thumbnails.default.url} />
         </div>
 
         <div className="media-body">
